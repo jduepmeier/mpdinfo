56f532e2c112e7710a28544b10a988a35e0567d3
Jan Düpmeier <j.duepmeier@googlemail.com>
2017-12-18 22:23:34 +0100

    reformat usage text

--- help.c
+++ help.c
@@ -9,20 +9,20 @@ int usage(int argc, char** argv, void* c) {
 	//options
 	printf("Usage: %s [options] <mode> [mode-args]\n", config->programName);
 	printf("Available options:\n");
-	printf("\t-c   | --config <config>\tPath to Config file\n");
-	printf("\t-f   | --format <string>\tFormat string for all\n");
-	printf("\t-fpa | --format=pause <string>\tFormat string for status paused\n");
-	printf("\t-fpl | --format=play <string>\tFormat string for status playing\n");
-	printf("\t-fs  | --format=stop <string>\tFormat string for status stopped\n");
-	printf("\t-hp  | --help\t\t\tShow this help\n");
-	printf("\t-h   | --host <host>\t\tSet the hostname\n");
-	printf("\t-p   | --port <port>\t\tSet the port\n");
-	printf("\t-v   | --verbosity <int>\tSet verbosity level between 0 (Error) and 5 (DEBUG)\n");
-	
+	printf("    -c  , --config <config>        Path to Config file\n");
+	printf("    -f  , --format <string>        Format string for all\n");
+	printf("    -fpa, --format=pause <string>  Format string for status paused\n");
+	printf("    -fpl, --format=play <string>   Format string for status playing\n");
+	printf("    -fs , --format=stop <string>   Format string for status stopped\n");
+	printf("    -hp , --help                   Show this help\n");
+	printf("    -h  , --host <host>            Set the hostname\n");
+	printf("    -p  , --port <port>            Set the port\n");
+	printf("    -v  , --verbosity <int>        Set verbosity level between 0 (Error) and 5 (DEBUG)\n");
+
 	//examples
 	printf("\nExamples:\n");
-	printf("%s --format \"%%artist%% - %%title%%\"\t\t returns the title and artist in format *artist* - *title*\n", config->programName);
-	
+	printf("%s --format \"%%artist%% - %%title%%\"    returns the title and artist in format *artist* - *title*\n", config->programName);
+
 	return -1;
 }
 
